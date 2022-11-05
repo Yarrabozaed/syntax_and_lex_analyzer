7371e0c3aeab566998c6ba9e6a2f18e4b0c8fc96
Yarrabozaed <yarraz3010@gmail.com>
2022-11-05 15:35:15 -0700

    added prolog code

--- rust-Yarrabozaed/target/debug/deps/rust_parser_assignment_02-f2e4b6932bb96e58.d
+++ rust-Yarrabozaed/target/debug/deps/rust_parser_assignment_02-f2e4b6932bb96e58.d
@@ -1,5 +1,5 @@
-/Users/Yarra/Desktop/lex-syn-analyzer/rust-Yarrabozaed/target/debug/deps/rust_parser_assignment_02-f2e4b6932bb96e58: src/main.rs
+/Users/Yarra/Documents/syntax_and_lex_analyzer/rust-Yarrabozaed/target/debug/deps/rust_parser_assignment_02-f2e4b6932bb96e58: src/main.rs
 
-/Users/Yarra/Desktop/lex-syn-analyzer/rust-Yarrabozaed/target/debug/deps/rust_parser_assignment_02-f2e4b6932bb96e58.d: src/main.rs
+/Users/Yarra/Documents/syntax_and_lex_analyzer/rust-Yarrabozaed/target/debug/deps/rust_parser_assignment_02-f2e4b6932bb96e58.d: src/main.rs
 
 src/main.rs:
--- rust-Yarrabozaed/target/debug/rust-parser-assignment-02.d
+++ rust-Yarrabozaed/target/debug/rust-parser-assignment-02.d
@@ -1,1 +1,1 @@
-/Users/Yarra/Desktop/lex-syn-analyzer/rust-Yarrabozaed/target/debug/rust-parser-assignment-02: /Users/Yarra/Desktop/lex-syn-analyzer/rust-Yarrabozaed/src/main.rs
+/Users/Yarra/Documents/syntax_and_lex_analyzer/rust-Yarrabozaed/target/debug/rust-parser-assignment-02: /Users/Yarra/Documents/syntax_and_lex_analyzer/rust-Yarrabozaed/src/main.rs
